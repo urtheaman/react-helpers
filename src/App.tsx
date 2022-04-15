f298dea8a6d0f28613681af8249ed9373e918b09
The Aman <urtheaman@gmail.com>
2022-04-15 15:44:48 +0530

    file structures component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import "./App.css";
 import fileStructures from "../filestructure.json";
-import { useState } from "react";
-
-interface Structure {
-  name: string;
-  files?: string[];
-  folders?: Structure[];
-}
+import Unfold from "./file-structures/Unfold";
 
 function App() {
   return (
@@ -16,31 +10,6 @@ function App() {
   );
 }
 
-const Unfold: React.FC<{ args: Structure }> = ({ args }): any => {
-  const [expand, setExpand] = useState(false);
-  if (args.name) {
-    return (
-      <>
-        {/* forlder names */}
-        <li
-          style={{ color: "red", listStyle: "none" }}
-          onClick={() => setExpand(!expand)}
-        >
-          {args.name}
-          <br />
-        </li>
 
-        <ul style={{ display: expand ? "block" : "none" }}>
-          {args?.folders?.map((el: any) => (
-            <Unfold key={el.name} args={el} />
-          ))}
-          {args?.files?.map((val) => (
-            <li>{val}</li>
-          ))}
-        </ul>
-      </>
-    );
-  } else return <li>{args.name}</li>; // file names
-};
 
 export default App;
