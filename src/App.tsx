a5120bdc44f2f98e684856cf2bd18b4dff16d0ca
The Aman <urtheaman@gmail.com>
2022-04-16 10:51:01 +0530

    modern redux implementation added

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import "./App.css";
 import fileStructures from "../filestructure.json";
 import Unfold from "./file-structures/Unfold";
 import Modal from "./modal/Modal";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Unfold args={fileStructures} />
-      <Modal>Hello There!</Modal>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={<h1>Complete Modern redux implementation</h1>}
+          />
+
+          <Route
+            path="file-structures"
+            element={<Unfold args={fileStructures} />}
+          />
+          <Route path="modal" element={<Modal>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod sed velit eum! Molestiae aliquam, cupiditate placeat eos reiciendis quia consequuntur soluta doloremque voluptatum maxime, pariatur aliquid officia totam quam magnam.</Modal>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
-
-
 
 export default App;
