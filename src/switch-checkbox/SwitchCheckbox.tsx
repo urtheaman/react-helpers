745e5be3e5ffb30154f54ab7e1731217276e543c
The Aman <urtheaman@gmail.com>
2022-05-02 12:16:03 +0530

    added switch checkbox

---
+++ src/switch-checkbox/SwitchCheckbox.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import styles from "./SwitchCheckbox.module.css";
+
+const SwitchCheckbox: NextPage<{
+  defaultChecked?: boolean;
+  onChange: any;
+  name?: string;
+  fontSize?: string;
+  fontWeight?: string | number;
+}> = ({
+  defaultChecked = false,
+  fontSize,
+  fontWeight,
+  name,
+  onChange,
+  ...otherProps
+}) => {
+  return (
+    <div>
+      {name && <span style={{ fontSize, fontWeight }}>{name}</span>}
+      <label className={styles.switch}>
+        <input
+          type="checkbox"
+          defaultChecked={defaultChecked}
+          onChange={onChange}
+          {...otherProps}
+        />
+        <span className={styles.slider}></span>
+      </label>
+    </div>
+  );
+};
+
+export default SwitchCheckbox;
